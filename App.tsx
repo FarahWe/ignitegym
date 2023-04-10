c9f270f43a2296d2cfd1fc20d7ca8c36e132b0cf
Farah <eduardobfarah@gmail.com>
2023-04-10 19:48:29 -0300

    Imagem de background

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { StatusBar, Text } from "react-native";
+import { StatusBar } from "react-native";
 import {
   useFonts,
   Roboto_400Regular,
@@ -8,6 +8,7 @@ import { NativeBaseProvider } from "native-base";
 
 import { Loading } from "@components/Loading";
 import { THEME } from "./src/theme";
+import { SignIn } from "@screens/SignIn";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -19,7 +20,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Text>Hello world</Text> : <Loading />}
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
