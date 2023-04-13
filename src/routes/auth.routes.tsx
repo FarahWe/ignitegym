08fe9fc5f4ec939be443485a7444c948959f926f
Farah <eduardobfarah@gmail.com>
2023-04-12 21:53:04 -0300

    Tipando Rotas

---
+++ src/routes/auth.routes.tsx
@@ -0,0 +1,24 @@
+import {
+  createNativeStackNavigator,
+  NativeStackNavigationProp,
+} from "@react-navigation/native-stack";
+import { SignIn } from "@screens/SignIn";
+import { SignUp } from "@screens/SignUp";
+
+type AuthRoutes = {
+  signIn: undefined;
+  signUp: undefined;
+};
+
+export type AuthNavigatorRoutesProps = NativeStackNavigationProp<AuthRoutes>;
+
+const { Navigator, Screen } = createNativeStackNavigator<AuthRoutes>();
+
+export function AuthRoutes() {
+  return (
+    <Navigator>
+      <Screen component={SignIn} name="signIn" />
+      <Screen component={SignUp} name="signUp" />
+    </Navigator>
+  );
+}
