e6dca8110f1c4090f82a29ad3a056c22bb784a63
Farah <eduardobfarah@gmail.com>
2023-04-13 20:27:25 -0300

    Finalizando o header da home

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,33 @@
+import { HStack, Heading, Icon, Text, VStack } from "native-base";
+
+import { MaterialIcons } from "@expo/vector-icons";
+
+import { UserPhoto } from "./UserPhoto";
+import { TouchableOpacity } from "react-native";
+
+export function HomeHeader() {
+  return (
+    <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+      <UserPhoto
+        alt="Imagem do usário"
+        mr={4}
+        size={16}
+        source={{ uri: "https://github.com/farahWe.png" }}
+      />
+
+      <VStack flex={1}>
+        <Text fontSize="md" color="gray.100">
+          Olá,
+        </Text>
+
+        <Heading fontSize="md" color="gray.100">
+          Eduardo Farah
+        </Heading>
+      </VStack>
+
+      <TouchableOpacity>
+        <Icon as={MaterialIcons} name="logout" color="gray.200" size={7} />
+      </TouchableOpacity>
+    </HStack>
+  );
+}
