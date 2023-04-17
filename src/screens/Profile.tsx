8b950ad2e45056b2022c4b009000e8c2ed45901a
Farah <eduardobfarah@gmail.com>
2023-04-16 21:17:07 -0300

    Carregamento de foto com o Skeleton

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,10 +1,36 @@
 import { ScreenHeader } from "@components/ScreenHeader";
-import { VStack } from "native-base";
+import { UserPhoto } from "@components/UserPhoto";
+import { Center, ScrollView, VStack, Skeleton } from "native-base";
+import { useState } from "react";
+
+const PHOTO_SIZE = 33;
 
 export function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false);
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
+
+      <ScrollView>
+        <Center mt={6} px={10}>
+          {photoIsLoading ? (
+            <Skeleton
+              h={PHOTO_SIZE}
+              w={PHOTO_SIZE}
+              rounded="full"
+              startColor="gray.500"
+              endColor="gray.400"
+            />
+          ) : (
+            <UserPhoto
+              source={{ uri: "https://github.com/FarahWe.png" }}
+              alt="foto do usuário"
+              size={PHOTO_SIZE}
+            />
+          )}
+        </Center>
+      </ScrollView>
     </VStack>
   );
 }
