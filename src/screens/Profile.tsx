4eefd6b9ba21ab779eb661f36a1e8b9fbbaaa4f0
Farah <eduardobfarah@gmail.com>
2023-05-05 08:34:34 -0300

    Finishing module 3

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -9,14 +9,57 @@ import {
   Skeleton,
   Text,
   KeyboardAvoidingView,
+  useToast,
 } from "native-base";
 import { useState } from "react";
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
 import { Platform, TouchableOpacity } from "react-native";
 
 const PHOTO_SIZE = 33;
 
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
+  const [userPhoto, setUserPhoto] = useState("https://github.com/FarahWe.png");
+
+  const toast = useToast();
+
+  async function handleUserPhotoSelected() {
+    setPhotoIsLoading(true);
+
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+
+      if (photoSelected.canceled) {
+        return;
+      }
+
+      const asset = photoSelected.assets[0];
+
+      if (asset) {
+        const photoInfo = await FileSystem.getInfoAsync(asset.uri);
+
+        if (photoInfo.exists && photoInfo.size / 1024 / 1024 > 2) {
+          return toast.show({
+            title: "Essa imagem é muito grande. Escolha uma de até 5MB.",
+            placement: "top",
+            bgColor: "red.500",
+          });
+        }
+
+        setUserPhoto(asset.uri);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setPhotoIsLoading(false);
+    }
+  }
 
   return (
     <VStack flex={1}>
@@ -30,29 +73,29 @@ export function Profile() {
           <Center mt={6} px={10}>
             {photoIsLoading ? (
               <Skeleton
-                h={PHOTO_SIZE}
                 w={PHOTO_SIZE}
+                h={PHOTO_SIZE}
                 rounded="full"
                 startColor="gray.500"
                 endColor="gray.400"
               />
             ) : (
               <UserPhoto
-                source={{ uri: "https://github.com/FarahWe.png" }}
-                alt="foto do usuário"
+                source={{ uri: userPhoto }}
+                alt="Foto do usuário"
                 size={PHOTO_SIZE}
               />
             )}
 
-            <TouchableOpacity>
+            <TouchableOpacity onPress={handleUserPhotoSelected}>
               <Text
                 color="green.500"
-                fontFamily="heading"
+                fontWeight="bold"
                 fontSize="md"
                 mt={2}
                 mb={8}
               >
-                Alterar foto
+                Alterar Foto
               </Text>
             </TouchableOpacity>
 
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -14,13 +14,52 @@ import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 import { Platform } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import { AuthNavigatorRoutesProps } from "@routes/auth.routes";
+import { useForm, Controller } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+type FormDataProps = {
+  name: string;
+  email: string;
+  password: string;
+  password_confirm: string;
+};
+
+const signUpSchema = yup.object({
+  name: yup.string().required("Informe o nome."),
+  email: yup.string().required("Informe o e-mail").email("E-mail inválido."),
+  password: yup
+    .string()
+    .required("Informe a senha")
+    .min(6, "A senha deve ter pelo menos 6 dígitos."),
+  password_confirm: yup
+    .string()
+    .required("Confirme a senha.")
+    .oneOf([yup.ref("password"), ""], "A confirmação da senha não confere"),
+});
 
 export function SignUp() {
-  const navigation = useNavigation<AuthNavigatorRoutesProps>();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormDataProps>({
+    resolver: yupResolver(signUpSchema),
+  });
 
-  function handleSignIn() {
-    navigation.navigate("signIn");
+  const navigation = useNavigation();
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
+  function handleSignUp({
+    name,
+    email,
+    password,
+    password_confirm,
+  }: FormDataProps) {
+    console.log({ name, email, password, password_confirm });
   }
 
   return (
@@ -37,12 +76,16 @@ export function SignUp() {
         behavior={Platform.OS == "ios" ? "padding" : undefined}
         keyboardVerticalOffset={20}
       >
-        <ScrollView showsVerticalScrollIndicator={true} flex={1}>
+        <ScrollView
+          showsVerticalScrollIndicator={true}
+          flex={1}
+          contentContainerStyle={{ paddingBottom: 100 }}
+        >
           <Center mt={10} mb={24}>
             <LogoSvg />
 
             <Text color="gray.100" fontSize="sm">
-              Treine sua mente e seu corpo
+              Treine sua mente e o seu corpo.
             </Text>
           </Center>
 
@@ -51,26 +94,76 @@ export function SignUp() {
               Crie sua conta
             </Heading>
 
-            <Input placeholder="Nome" autoCapitalize="none" />
-
-            <Input
-              placeholder="Email"
-              keyboardType="email-address"
-              autoCapitalize="none"
+            <Controller
+              control={control}
+              name="name"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="Nome"
+                  onChangeText={onChange}
+                  value={value}
+                  errorMessage={errors.name?.message}
+                />
+              )}
             />
 
-            <Input placeholder="Senha" secureTextEntry />
+            <Controller
+              control={control}
+              name="email"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="E-mail"
+                  keyboardType="email-address"
+                  autoCapitalize="none"
+                  onChangeText={onChange}
+                  value={value}
+                  errorMessage={errors.email?.message}
+                />
+              )}
+            />
 
-            <Button title="Criar e acessar" />
-          </Center>
+            <Controller
+              control={control}
+              name="password"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="Senha"
+                  secureTextEntry
+                  onChangeText={onChange}
+                  value={value}
+                  errorMessage={errors.password?.message}
+                />
+              )}
+            />
 
-          <Center mt={24} pb={10}>
+            <Controller
+              control={control}
+              name="password_confirm"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="Confirmar a Senha"
+                  secureTextEntry
+                  onChangeText={onChange}
+                  value={value}
+                  onSubmitEditing={handleSubmit(handleSignUp)}
+                  returnKeyType="send"
+                  errorMessage={errors.password_confirm?.message}
+                />
+              )}
+            />
+
             <Button
-              title="Voltar para login"
-              variant="outline"
-              onPress={handleSignIn}
+              title="Criar e acessar"
+              onPress={handleSubmit(handleSignUp)}
             />
           </Center>
+
+          <Button
+            title="Voltar para o login"
+            variant="outline"
+            mt={12}
+            onPress={handleGoBack}
+          />
         </ScrollView>
       </KeyboardAvoidingView>
     </VStack>
